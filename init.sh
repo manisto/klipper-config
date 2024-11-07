6dac555f85f53836b1c7ef1b8285e9f4dee28b01
Gert Rue Brigsted <grbrigsted@gmail.com>
2024-11-07 15:33:45 +0100

    Made init.sh idempotent.

--- init.sh
+++ init.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-ln -s "./$1/crowsnest.conf"
-ln -s "./$1/moonraker.conf"
-ln -s "./$1/printer.cfg"
+
+if [ -z "$1" ]
+then
+    echo 'You must specify the config to init, like this:'
+    echo '  ./init.sh v2.4'
+    exit 1
+fi
+
+function init_file() {
+    if [ -f "$2" ]; then
+        if [ ! -L "$2" ]; then
+            cp "$2" "./$1/$2"
+        fi
+
+        rm "$2"
+    fi
+
+    ln -s "./$1/$2"
+}
+
+init_file $1 crowsnest.conf
+init_file $1 moonraker.conf
+init_file $1 printer.cfg
